5951c51749e59683f7d8d9893156eadfb7cdf662
Ivan Markin <git@unkaktus.art>
2024-07-22 23:39:46 +0200

    Remove Cobra switch, as it is no longer operational

--- rewrite_node.go
+++ rewrite_node.go
@@ -11,18 +11,10 @@ func isSupermuc() bool {
 	return strings.Contains(string(combi), "sng.lrz.de")
 }
 
-func isCobra() bool {
-	cmd := exec.Command("hostname")
-	combi, _ := cmd.CombinedOutput()
-	return strings.Contains(string(combi), "cobra")
-}
-
 func RewriteNode(node string) string {
 	switch {
 	case isSupermuc():
 		return node + "opa"
-	case isCobra():
-		return node + "-opa"
 	}
 	return node
 }
